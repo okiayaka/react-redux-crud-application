c7a4d1881b892a56ee01200182d1b097b7d0356d
ayaka oki <krcklcp10nknk@gmail.com>
2023-03-18 16:32:00 +0900

    learn props for component

--- src/App.js
+++ src/App.js
@@ -11,17 +11,28 @@ import React, { Component } from "react";
 // }
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10},
+    { name: "Hanako", age: 15},
+    { name: "NoName"},
+  ]
   return (
   <div>
-    <Cat />
-    <Cat />
-    <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />;
+        })
+      }
   </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name} , and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 0
 }
 
 export default App;
